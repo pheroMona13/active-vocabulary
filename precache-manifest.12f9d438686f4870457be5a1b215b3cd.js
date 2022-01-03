044055b55ad061e9587736afaebb8d70679c936e
Kefayati <pheromona13@gmail.com>
2022-01-03 12:25:42 +0330

    init

---
+++ precache-manifest.12f9d438686f4870457be5a1b215b3cd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1788483bc8667d8d666b405443d3404",
+    "url": "/active-vocabulary/index.html"
+  },
+  {
+    "revision": "9f4a3ac790e3c468ba57",
+    "url": "/active-vocabulary/static/css/main.e26f67a8.chunk.css"
+  },
+  {
+    "revision": "a53db2b9d3bdc4ad0c5f",
+    "url": "/active-vocabulary/static/js/2.f4aa5786.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/active-vocabulary/static/js/2.f4aa5786.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f4a3ac790e3c468ba57",
+    "url": "/active-vocabulary/static/js/main.ca8240e4.chunk.js"
+  },
+  {
+    "revision": "9df984ccc2d323a37904",
+    "url": "/active-vocabulary/static/js/runtime-main.0aee4390.js"
+  },
+  {
+    "revision": "8fe6c73220785b9ea7d77ec216952fa8",
+    "url": "/active-vocabulary/static/media/google-logo.8fe6c732.png"
+  },
+  {
+    "revision": "30a8256f6a260d95868dbaba9c1c02a4",
+    "url": "/active-vocabulary/static/media/translate-logo.30a8256f.png"
+  }
+]);
